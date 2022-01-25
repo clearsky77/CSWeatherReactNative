dcf35813ff5c6a99063756d6cb8a0c065c5afabc
clearsky77 <clearsky3264@gmail.com>
2022-01-25 19:20:45 +0900

    styleSheet를 적용해보기  - 폰트 사이즈 조정

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!</Text>
+      <Text style={styles.Text}>Hello World!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    fontSize: 28,
   },
 });
