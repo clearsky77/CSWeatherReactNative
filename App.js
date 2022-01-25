70ac1ae1b98d66621ed2e6f602bd23ef7ed3864c
clearsky77 <clearsky3264@gmail.com>
2022-01-25 20:20:29 +0900

    react-native에 없는 기능(API)을 expo로 쓰기

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello!!!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      {/* 여기에 light는 react-native에서가 아닌 expo의 API를 쓴 것이다. */}
     </View>
   );
 }
