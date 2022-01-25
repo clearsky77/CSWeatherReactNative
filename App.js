deb93d6bf0ac83face552691076b55badb5e7759
clearsky77 <clearsky3264@gmail.com>
2022-01-25 20:37:30 +0900

    FlexBox를 쓴 후

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <View style={{width:100, height:100, backgroundColor: "orange"}}></View>
-      <View style={{width:100, height:100, backgroundColor: "green"}}></View>
-      <View style={{width:100, height:100, backgroundColor: "red"}}></View>
+    <View style={{flex: 1}}>
+      <View style={{flex: 1, backgroundColor: "orange"}}></View>
+      <View style={{flex: 1, backgroundColor: "green"}}></View>
+      <View style={{flex: 1, backgroundColor: "red"}}></View>
     </View>
   );
 }
