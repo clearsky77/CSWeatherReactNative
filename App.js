ad63dac972e93845c528cbcfb2cf13e1ccbb08f3
clearsky77 <clearsky3264@gmail.com>
2022-01-25 20:32:08 +0900

    FlexBox를 쓰기 전

--- App.js
+++ App.js
@@ -3,25 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello!!!</Text>
-      <StatusBar style="light" />
-      {/* 여기에 light는 react-native에서가 아닌 expo의 API를 쓴 것이다. */}
+    <View>
+      <View style={{width:100, height:100, backgroundColor: "orange"}}></View>
+      <View style={{width:100, height:100, backgroundColor: "green"}}></View>
+      <View style={{width:100, height:100, backgroundColor: "red"}}></View>
     </View>
   );
 }
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    fontSize: 28,
-    color: "red",
-  },
-});
