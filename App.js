6784ece3904cacb8e2dff61d05301493d2baa55b
clearsky77 <clearsky3264@gmail.com>
2022-01-26 18:14:24 +0900

    위치 정보(Location) 1.설치 2.import 3.권한 허용 팝업

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, ScrollView, Dimensions, StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import * as Location from "expo-location";
 
-const { width:SCREEN_WIDTH } = Dimensions.get("window");
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 // const SCREEN_WIDTH = Dimensions.get("window").width; // 이렇게도 가능
 
-console.log(SCREEN_WIDTH);
-
 export default function App() {
+
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+
+  const ask = async () => {
+    const permission = await Location.requestForegroundPermissionsAsync();
+    console.log(permission);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
         <Text style={styles.cityName}>Pusan</Text>
       </View>
       <ScrollView
-      pagingEnabled
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      contentContainerStyle={styles.weather}>
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}>
         <View style={styles.day}>
           <Text style={styles.temp}>13</Text>
           <Text style={styles.description}>Sunny</Text>
