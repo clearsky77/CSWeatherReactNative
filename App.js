6c25c732be51d9dcdb9c49c6e904e51575bd79c3
clearsky77 <clearsky3264@gmail.com>
2022-01-26 13:23:33 +0900

    UI 지역 칸을 배치함

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  return <View style={styles.container}>
+    <View style={styles.city}>
+      <Text style={styles.cityName}>Pusan</Text>
+    </View>
+    <View style={styles.weather}>
+    </View>
+  </View>;
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    backgroundColor: "azure",
+    backgroundColor: "#DEB887",
   },
+  city:{
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName:{
+    fontSize: 35,
+    fontWeight: "400",
+  },
+  weather:{
+    flex: 3,
+    // backgroundColor: "teal", // 위치를 조정할 때는 색을 입혀서 전체 크기를 확인하자
+  }
 });
